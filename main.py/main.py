55e9143b573ebae153494b38468010c468c21d30
Joel Mugambwa <khezajoel@gmail.com>
2024-02-15 14:35:33 +0200

    Add project files.

---
+++ main.py/main.py
@@ -0,0 +1,25 @@
+﻿import tkinter as tk
+from time import strftime
+
+
+root = tk.Tk()
+root.title("Digital Clock")
+root.resizable(False, False)
+root.geometry("550x340")
+
+
+
+
+def time():
+    tyme = strftime("%I:%M:%S %p")
+    lbl.config(text=tyme)
+    lbl.after(1000, time)
+
+lbl = tk.Label(root, font=('franklin go', 50), bg="black", fg="green")
+lbl.pack(expand=True)
+
+time()
+
+root.mainloop()
+
+
